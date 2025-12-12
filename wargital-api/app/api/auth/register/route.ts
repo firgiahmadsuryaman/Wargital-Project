58e16375f6c7b081073a99d30c73a3f3debb0ba7
firgiahmadsuryaman <firgi_ahmad_suryaman@teknokrat.ac.id>
2025-12-12 20:19:17 +0700

    Menambahkan baris code Validasi input untuk registrasi di route.ts (auth/register)

--- wargital-api/app/api/auth/register/route.ts
+++ wargital-api/app/api/auth/register/route.ts
@@ -3,3 +3,9 @@ import { prisma } from '@/lib/prisma';
 import { z } from 'zod';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+
+// Validasi input untuk registrasi
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
