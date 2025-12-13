48794f8c6a78f17565ca4a66799ba398a6469a5e
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2025-12-14 01:41:17 +0700

    Menambahkan komponen RootLayout dengan pengaturan dasar dan penyedia konteks

--- wargital-webiste/src/app/layout.tsx
+++ wargital-webiste/src/app/layout.tsx
@@ -12,4 +12,36 @@ export const metadata: Metadata = {
   description: 'Rasa masakan rumah, diantar ke pintu Anda.',
 };
 
-
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="font-body antialiased pb-20 md:pb-0">
+        <AuthProvider>
+          <FavoritesProvider>
+            <CartProvider>
+              <div className="min-h-screen flex flex-col">
+                <Header />
+                <main className="flex-grow">
+                  {children}
+                </main>
+              </div>
+              <Toaster />
+            </CartProvider>
+          </FavoritesProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
