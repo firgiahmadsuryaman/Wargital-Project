917fa05b18eed410c50ad68b76142b78f78d796c
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2025-12-17 00:55:15 +0700

    Membuat layanan autentikasi untuk registrasi

--- wargital-mobile/services/authService.ts
+++ wargital-mobile/services/authService.ts
@@ -4,5 +4,9 @@ export const authService = {
     login: async (email, password) => {
         const response = await api.post('/auth/login', { email, password });
         return response.data;
+    },
+    register: async (email, password) => {
+        const response = await api.post('/auth/register', { email, password });
+        return response.data;
     }
 };
