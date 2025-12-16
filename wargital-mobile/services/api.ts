85e5369c9b7f29eb72814d6bcb9e9c3ba7fcff97
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2025-12-17 00:50:08 +0700

    Menambahkan konfigurasi URL API

--- wargital-mobile/services/api.ts
+++ wargital-mobile/services/api.ts
@@ -1,4 +1,11 @@
 import axios from 'axios';
 import { Platform } from 'react-native';
 
+// Use 10.0.2.2 for Android Emulator, localhost for iOS Simulator
+const API_URL = Platform.select({
+    ios: 'http://localhost:3000/api', // Simulator
+    android: 'http://192.168.100.10:3000/api', // LAN IP (Use this instead of 10.0.2.2)
+    default: 'http://localhost:3000/api',
+});
+
 
