f173d49662b791888b501f3e4ef2a30d6d73efc3
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2025-12-17 01:12:34 +0700

    Menambahkan konteks otentikasi dengan AuthContext untuk mengelola status pengguna

---
+++ wargital-mobile/context/AuthContext.tsx
@@ -0,0 +1,6 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter, useSegments } from 'expo-router';
+import api from '@/services/api';
+
+
