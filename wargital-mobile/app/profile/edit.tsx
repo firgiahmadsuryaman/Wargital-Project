7c776a06d13aa07ae1494fce7d7ccc3cce378342
firgiahmadsuryaman <firgi_ahmad_suryaman@teknokrat.ac.id>
2026-01-17 20:38:33 +0700

    membuat tampilan menu edit profil & inisialisasi struktur dasar dan import library EditProfileScreen

---
+++ wargital-mobile/app/profile/edit.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, TextInput, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Stack, useRouter } from 'expo-router';
+import { useState } from 'react';
+import { useAuth } from '@/context/AuthContext';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function EditProfileScreen() {
+    return null;
+}
+
+const styles = StyleSheet.create({});
