9bf0b8ec56030833720e527ae0f658fa3df507b8
firgiahmadsuryaman <firgi_ahmad_suryaman@teknokrat.ac.id>
2026-01-17 21:05:46 +0700

    impor library react, native, router, dan service alamat

---
+++ wargital-mobile/app/profile/address/form.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, View, TextInput, TouchableOpacity, ScrollView, Alert, ActivityIndicator } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { useState, useEffect } from 'react';
+import { addressService } from '@/services/addressService';
+import { Address } from '@/types';
+import { Ionicons } from '@expo/vector-icons';
