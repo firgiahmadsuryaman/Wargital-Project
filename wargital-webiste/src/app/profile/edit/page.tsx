d0a1a0786a9bca2a4cb2638e5f10a0e25464e5d3
firgiahmadsuryaman <firgi_ahmad_suryaman@teknokrat.ac.id>
2026-01-17 22:59:13 +0700

    tambahkan kondisi loading spinner saat memuat data user

--- wargital-webiste/src/app/profile/edit/page.tsx
+++ wargital-webiste/src/app/profile/edit/page.tsx
@@ -48,3 +48,11 @@ export default function EditProfilePage() {
             setIsLoading(false);
         }
     };
+
+    if (isUserLoading || !user) {
+        return (
+            <div className="flex h-[calc(100vh-4rem)] items-center justify-center">
+                <Loader2 className="h-8 w-8 animate-spin text-primary" />
+            </div>
+        );
+    }
