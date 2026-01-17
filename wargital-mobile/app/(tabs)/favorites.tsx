8924a42116480ae21e4f2f10645b24f9f8fe50df
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2026-01-17 21:27:46 +0700

    impor library react, native, service favorit, dan komponen kartu

---
+++ wargital-mobile/app/(tabs)/favorites.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, FlatList, View, RefreshControl, ActivityIndicator } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { RestaurantCard } from '@/components/RestaurantCard';
+import { useState, useCallback } from 'react';
+import { Restaurant } from '@/types';
+import { favoriteService } from '@/services/favoriteService';
+import { useFocusEffect, useRouter } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
