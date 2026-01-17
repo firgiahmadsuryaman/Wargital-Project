38ed3a3e0e40050ee93cda1ff27f66fe3c3b65be
andrepratama24 <muhamad_andre_pratama@teknokrat.ac.id>
2026-01-17 23:11:23 +0700

    membuat file page.tsx untuk metode pembyaran

---
+++ wargital-webiste/src/app/profile/payment/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { ArrowLeft, CreditCard, Plus, Trash2, Wallet } from 'lucide-react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
